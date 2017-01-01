597903284bebe7c036ef15588388900790b576de
soruma <soruma.net@gmail.com>
2017-01-01 10:18:06 +0900

    本と貸出、ユーザを関連付け#4

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_and_belongs_to_many :lends
+
   mount_uploader :image, ImageUploader
   mount_uploader :bookpdf, PdfUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :roles
+  has_and_belongs_to_many :lends
 
   def has_role?(name)
     self.roles.where(name: name).length > 0
