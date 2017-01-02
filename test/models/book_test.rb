944251ff65daf2c6f8dec9bbf99c63f34275d32d
soruma <soruma.net@gmail.com>
2017-01-02 23:38:36 +0900

    Book テスト修正

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
+require_relative '../test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has make recode" do
+    file_dir = CarrierWave.root #あとで消してみる
+    File.open(file_dir.join(book.image.file.path)) do |img|
+      book.image = img
+    end
+    File.open(file_dir.join(book.bookpdf.file.path)) do |pdf|
+      book.bookpdf = pdf
+    end
+    book.save!
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,18 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include ActionDispatch::TestProcess
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # Not storing uploads in the tests
+  CarrierWave::Mount::Mounter.class_eval { def store!; end }
+  CarrierWave.root = Rails.root.join('test/fixtures/files/uploads')
+
   # Add more helper methods to be used by all tests here...
+  def after_teardown
+    super
+    CarrierWave.clean_cached_files!(0)
+  end
 end
