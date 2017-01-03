9e473f37414c1d5afaa97736dcc3e76663259ae3
soruma <soruma.net@gmail.com>
2017-01-03 20:48:54 +0900

    create test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = Role.create(:name => 'admin')
-superadmin = Role.create(:name => 'superadmin')
+admin = Role.find_or_create_by(:name => 'admin')
+superadmin = Role.find_or_create_by(:name => 'superadmin')
 
-User.create(:email => 'admin@ex.com',
-            :password => 'adminadmin',
-            :password_confirmation => 'adminadmin',
-            :roles => [ admin ])
+User.find_or_create_by(:email => 'superadmin@ex.com') do |user|
+  user.password = 'adminadmin'
+  user.password_confirmation = 'adminadmin'
+  user.roles = [ admin, superadmin ]
+end
 
-User.create(:email => 'superadmin@ex.com',
-            :password => 'adminadmin',
-            :password_confirmation => 'adminadmin',
-            :roles => [ admin, superadmin ])
+User.find_or_create_by(:email => 'admin@ex.com') do |user|
+  user.password = 'adminadmin'
+  user.password_confirmation = 'adminadmin'
+  user.roles = [ admin ]
+end
 
-User.create(:email => 'user@ex.com',
-            :password => 'useruser',
-            :password_confirmation => 'useruser')
+User.find_or_create_by(:email => 'user@ex.com') do |user|
+  user.password = 'useruser'
+  user.password_confirmation = 'useruser'
+end
+
+file_path = "#{Rails.root}/db/data/"
+
+Book.find_or_create_by(:title => 'test 1') do |book|
+  book.author = 'author 1'
+  book.publisher = 'publisher 1'
+  book.image = open(file_path + "test1.jpg")
+  book.bookpdf = open(file_path + "test.pdf")
+end
+
+Book.find_or_create_by(:title => 'test 2') do |book|
+  book.author = 'author 2'
+  book.publisher = 'publisher 2'
+  book.image = open(file_path + "test2.jpg")
+  book.bookpdf = open(file_path + "test.pdf")
+end
+
+Book.find_or_create_by(:title => 'test 3') do |book|
+  book.author = 'author 3'
+  book.publisher = 'publisher 3'
+  book.bookpdf = open(file_path + "test.pdf")
+end
