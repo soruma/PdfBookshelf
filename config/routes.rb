ecb1a4efd64ba8c0e6d845c1e90d0937c57407a6
soruma <soruma.net@gmail.com>
2017-01-03 10:40:21 +0900

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'information/index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  get 'information/index'
 
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'information#index'
+  root :to => 'home#index'
 end
